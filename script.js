04875383e99266c5617cbee210d59c0c46d14edb
yuukaaa <79951128+yuukaaa@users.noreply.github.com>
2021-04-26 13:47:34 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-    function openNav() {
-    document.getElementById("sidepanel").style.width = "250px";
-    }
-
-    function closeNav() {
-    document.getElementById("sidepanel").style.width = "0";
-    }
