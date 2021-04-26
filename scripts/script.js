a9438ec92453ad7cb6b8542f9cb980145869c258
yuukaaa <79951128+yuukaaa@users.noreply.github.com>
2021-04-26 13:48:20 -0400

    added files

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+
+    function openNav() {
+    document.getElementById("sidepanel").style.width = "250px";
+    }
+
+    function closeNav() {
+    document.getElementById("sidepanel").style.width = "0";
+    }
