4fbe040e1e19bed896146ba1ca588c115a5a6037
yuukaaa <79951128+yuukaaa@users.noreply.github.com>
2021-04-27 18:39:57 -0400

    fixed accordion

--- scripts/script.js
+++ scripts/script.js
@@ -7,4 +7,28 @@
     document.getElementById("sidepanel").style.width = "0";
     }
 
-    new Accordion(".accordion-container");
+   
+    var ac = document.getElementsByClassName("trigger");
+    var i;
+    
+    for (i = 0; i < ac.length; i++) {
+      ac[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+    
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          }
+      });
+    }
+   
