a5da04bdf540316d62b313a9d53895919b49622f
yuukaaa <79951128+yuukaaa@users.noreply.github.com>
2021-05-01 15:02:46 -0400

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -13,11 +13,9 @@
     
     for (i = 0; i < ac.length; i++) {
       ac[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
+
         this.classList.toggle("active");
     
-        /* Toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
         if (panel.style.display === "block") {
           panel.style.display = "none";
